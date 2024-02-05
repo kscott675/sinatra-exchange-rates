1a5bbb94908f9af4bd65f654dbe9353cbaaf545d
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-02-05 04:48:08 +0000

    app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,43 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'http'
+require 'json'
+
+EXCHANGE_API_KEY = ENV['EXCHANGE_API_KEY']
+api_url = nil
+
+def fetch_currency_list
+  api_url = "http://api.exchangerate.host/list?access_key=#{EXCHANGE_API_KEY}"
+  raw_data = HTTP.get(api_url)
+  JSON.parse(raw_data.to_s)["currencies"].keys
+end
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @symbols = fetch_currency_list
+  erb :homepage
 end
+
+# Define a route for individual currency conversion
+get("/convert/:from_currency") do
+  @from_currency = params["from_currency"]
+  @symbols = fetch_currency_list
+  erb :convert_currency
+end
+
+# Define a route for specific currency pair conversion
+  get("/convert/:from_currency/:to_currency") do
+    @from_currency = params.fetch("from_currency")
+    @to_currency = params.fetch("to_currency")
+  
+    @conversion_url = "http://api.exchangerate.host/convert?access_key=#{EXCHANGE_API_KEY}&from=#{@from_currency}&to=#{@to_currency}&amount=1"
+  
+    @conversion_data = JSON.parse(HTTP.get(@conversion_url).to_s)
+  
+    if @conversion_data["success"]
+      @conversion_rate = @conversion_data["result"]
+      erb :specific_conversion
+    else
+      @error_message = "Unable to fetch conversion rate."
+      erb :error_page
+    end
+  end
